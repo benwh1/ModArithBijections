23750caf945ee4a31f43ef787a835812179cde52
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-10-31 23:51:11 +0000

    Rename project

--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,12 @@
 import Lake
 open Lake DSL
 
-package «testing» where
+package «mod-arith-bijections» where
   -- add any package configuration options here
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
 @[default_target]
-lean_lib «Testing» where
+lean_lib «ModArithBijections» where
   -- add any library configuration options here
