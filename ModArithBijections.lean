99b056641196dc2ab9e340fbdcbe48858c0508e1
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-11-05 22:03:58 +0000

    Update module names

--- ModArithBijections.lean
+++ ModArithBijections.lean
@@ -1,3 +1,4 @@
 -- This module serves as the root of the `ModArithBijections` library.
 -- Import modules here that should be built as part of the library.
-import «ModArithBijections».Basic
+import ModArithBijections.Helper
+import ModArithBijections.ModM
