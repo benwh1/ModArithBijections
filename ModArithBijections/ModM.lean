c296486db3901cd59ca8d6bb0fef553e59dcb4ed
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-11-06 03:28:05 +0000

    Generalize theorem to a*b^n+n

--- ModArithBijections/ModM.lean
+++ ModArithBijections/ModM.lean
@@ -1,17 +1,17 @@
 import Mathlib.FieldTheory.Finite.Basic
 import ModArithBijections.Helper
 
-def f (a x : ℕ) : ℕ := a * 3 ^ x + x
+def f (a b x : ℕ) : ℕ := a * b ^ x + x
 
 noncomputable def order (m n : ℕ) := orderOf (n : ZMod m)
 
 @[simp]
-noncomputable def seq m n := match n with
+noncomputable def seq b m n := match n with
   | 0 => m
-  | Nat.succ k => order (seq m k) 3
+  | Nat.succ k => order (seq b m k) b
 
-lemma order_lt {m} (hm : m > 1) (hcoprime : Nat.Coprime 3 m) : order m 3 < m := by
-  have ⟨k, hk1, hkm, hk⟩ : ∃ k, 1 <= k ∧ k < m ∧ 3^k ≡ 1 [MOD m] := by
+lemma order_lt {m} (hm : m > 1) (hcoprime : Nat.Coprime b m) : order m b < m := by
+  have ⟨k, hk1, hkm, hk⟩ : ∃ k, 1 <= k ∧ k < m ∧ b^k ≡ 1 [MOD m] := by
     use Nat.totient m
     have hm0 : m > 0 := Nat.zero_lt_of_lt hm
     apply And.intro
@@ -23,60 +23,54 @@ lemma order_lt {m} (hm : m > 1) (hcoprime : Nat.Coprime 3 m) : order m 3 < m := 
     }
   apply (ZMod.eq_iff_modEq_nat m).mpr at hk
   simp at hk
-  have hk : order m 3 <= k := orderOf_le_of_pow_eq_one hk1 hk
+  have hk : order m b <= k := orderOf_le_of_pow_eq_one hk1 hk
   exact Nat.lt_of_le_of_lt hk hkm
 
-lemma order_dvd {m x} (hx : 3^x ≡ 1 [MOD m]) : order m 3 ∣ x := by
+lemma order_dvd {m x} (hx : b^x ≡ 1 [MOD m]) : order m b ∣ x := by
   unfold order
-  simp
-  have h : (3 : ZMod m)^x = 1 := by
+  have h : (b : ZMod m)^x = 1 := by
     apply (ZMod.eq_iff_modEq_nat m).mpr at hx
     simp at hx
     exact hx
   exact orderOf_dvd_of_pow_eq_one h
 
-lemma pow_ndvd {m k} (hk : k > 0) (h : 3^k ≡ 1 [MOD m]) : Nat.Coprime 3 m := by
-  have hprime : Nat.Prime 3 := by norm_num
-  apply (Nat.Prime.coprime_iff_not_dvd hprime).mpr
-  intro hdvd
+lemma pow_ndvd {m k} (hk : k > 0) (h : b^k ≡ 1 [MOD m]) : Nat.Coprime b m := by
+  have hk1 : k.pred + 1 = k := Nat.succ_pred_eq_of_pos hk
+  apply Nat.coprime_of_mul_modEq_one (b^k.pred)
+  rw [mul_comm]
+  have h1 := pow_add b (k.pred) 1
+  simp at h1
+  rw [← h1, hk1]
+  exact h
 
-  have ⟨d, hd⟩ := hdvd
-  rw [hd] at h
-  apply Nat.ModEq.of_mul_right at h
-  have hdvd2 : 3 ∣ 3^k := pow_dvd_pow 3 hk
-  have h0 := Nat.modEq_zero_iff_dvd.mpr hdvd2
-  have h01 := Nat.ModEq.trans (h.symm) h0
-  simp at h01
-
-lemma seq_pow_mod_prev m k : 3^seq m k.succ ≡ 1 [MOD seq m k] := by
+lemma seq_pow_mod_prev b m k : b^seq b m k.succ ≡ 1 [MOD seq b m k] := by
   apply (ZMod.eq_iff_modEq_nat _).mp
   simp
   apply pow_orderOf_eq_one
 
-lemma order_iter {m} (h : 3^m ≡ 1 [MOD m]) : 3^order m 3 ≡ 1 [MOD order m 3] := by
-  have hm : 3^order m 3 ≡ 1 [MOD m] := seq_pow_mod_prev m 0
-  have ⟨k, hk⟩ : order m 3 ∣ m := by
+lemma order_iter {b m} (h : b^m ≡ 1 [MOD m]) : b^order m b ≡ 1 [MOD order m b] := by
+  have hm : b^order m b ≡ 1 [MOD m] := seq_pow_mod_prev b m 0
+  have ⟨k, hk⟩ : order m b ∣ m := by
     apply orderOf_dvd_iff_pow_eq_one.mpr
-    simp
     have h1 := (ZMod.eq_iff_modEq_nat m).mpr h
     simp at h1
     exact h1
   nth_rewrite 1 [hk] at hm
   exact Nat.ModEq.of_mul_right k hm
 
-lemma seq_pow_one {m} (h : 3^m ≡ 1 [MOD m]) : ∀ k, 3^seq m k ≡ 1 [MOD seq m k] := by
+lemma seq_pow_one {b m} (h : b^m ≡ 1 [MOD m]) : ∀ k, b^seq b m k ≡ 1 [MOD seq b m k] := by
   intro k
   induction' k with k hk
   { exact h }
   { exact order_iter hk }
 
-lemma seq_dvd_prev {m} (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k.succ ∣ seq m k := by
+lemma seq_dvd_prev {b m} (h : b^m ≡ 1 [MOD m]) : ∀ k, seq b m k.succ ∣ seq b m k := by
   intro k
   simp
   apply order_dvd
   apply seq_pow_one h
 
-lemma seq_dvd_seq_zero {m} (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k ∣ m := by
+lemma seq_dvd_seq_zero {b m} (h : b^m ≡ 1 [MOD m]) : ∀ k, seq b m k ∣ m := by
   intro k
   induction' k with k hk
   { simp }
@@ -85,25 +79,25 @@ lemma seq_dvd_seq_zero {m} (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k ∣ m := by
     exact Nat.dvd_trans h1 hk
   }
 
-lemma seq_pos {m} (hm : m > 0) (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k > 0 := by
+lemma seq_pos {b m} (hm : m > 0) (h : b^m ≡ 1 [MOD m]) : ∀ k, seq b m k > 0 := by
   intro k
   induction' k with k hk
   { exact hm }
   {
     simp
-    have hpow : (3 : ZMod (seq m k))^seq m k = 1 := by
+    have hpow : (b : ZMod (seq b m k))^seq b m k = 1 := by
       have h1 := seq_pow_one h k
       have h2 := (ZMod.eq_iff_modEq_nat _).mpr h1
       simp at h2
       exact h2
-    have hpow' : 3^seq m k ≡ 1 [MOD seq m k] := by
-      apply (ZMod.eq_iff_modEq_nat (seq m k)).mp
+    have hpow' : b^seq b m k ≡ 1 [MOD seq b m k] := by
+      apply (ZMod.eq_iff_modEq_nat (seq b m k)).mp
       simp
       exact hpow
-    have hcoprime : Nat.Coprime 3 (seq m k) := pow_ndvd hk hpow'
-    let three := ZMod.unitOfCoprime 3 hcoprime
-    have _ : NeZero (seq m k) := NeZero.of_gt hk
-    have _ : Finite (ZMod (seq m k)) := by
+    have hcoprime : Nat.Coprime b (seq b m k) := pow_ndvd hk hpow'
+    let three := ZMod.unitOfCoprime b hcoprime
+    have _ : NeZero (seq b m k) := NeZero.of_gt hk
+    have _ : Finite (ZMod (seq b m k)) := by
       apply Fintype.finite
       apply ZMod.fintype
     have h := orderOf_pos three
@@ -111,39 +105,39 @@ lemma seq_pos {m} (hm : m > 0) (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k > 0 := b
     exact h
   }
 
-lemma seq_dec {m k} (hm : m > 0) (h : 3^m ≡ 1 [MOD m]) (hg1 : seq m k > 1)
-  : seq m k.succ < seq m k := by
-  have hcoprime : Nat.Coprime 3 (seq m k) := by
+lemma seq_dec {b m k} (hm : m > 0) (h : b^m ≡ 1 [MOD m]) (hg1 : seq b m k > 1)
+  : seq b m k.succ < seq b m k := by
+  have hcoprime : Nat.Coprime b (seq b m k) := by
     apply pow_ndvd hm
     have ⟨d, hd⟩ := seq_dvd_seq_zero h k
     nth_rewrite 1 [hd] at h
     exact Nat.ModEq.of_mul_right d h
   apply order_lt hg1 hcoprime
 
-lemma seq_lt_seq_zero {m} (hm : m > 0) (h : 3^m ≡ 1 [MOD m]) : ∀ k, seq m k <= m := by
+lemma seq_lt_seq_zero {b m} (hm : m > 0) (h : b^m ≡ 1 [MOD m]) : ∀ k, seq b m k <= m := by
   intro k
   have hdvd := seq_dvd_seq_zero h k
   exact Nat.le_of_dvd hm hdvd
 
-lemma seq_one_next {m} (h : seq m k = 1) : seq m k.succ = 1 := by
+lemma seq_one_next {b m} (h : seq b m k = 1) : seq b m k.succ = 1 := by
   simp
   rw [h]
   unfold order
   simp
 
-lemma seq_one {m} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) : ∃ k, seq m k = 1 := by
+lemma seq_one {b m} (hmgt : m > 0) (hm : b^m ≡ 1 [MOD m]) : ∃ k, seq b m k = 1 := by
   let r := Nat.lt
-  let C := fun x => ∃ k, seq m k <= x
+  let C := fun x => ∃ k, seq b m k <= x
   have : WellFounded r := by apply Nat.lt_wfRel.wf
   have well_ordering := @WellFounded.induction_bot ℕ r this m 1 C
   unfold_let at well_ordering
   simp at well_ordering
 
-  have seq_le_1 : ∃ k, seq m k <= 1 := by
+  have seq_le_1 : ∃ k, seq b m k <= 1 := by
     apply well_ordering
     {
-      intro b hb x hx
-      use (seq m x.succ)
+      intro u hu x hx
+      use (seq b m x.succ)
       apply And.intro
       {
         have h0 : ∀ n, n = 0 ∨ n = 1 ∨ n > 1 := by
@@ -151,7 +145,7 @@ lemma seq_one {m} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) : ∃ k, seq m k = 1 :
           cases' n with n
           { simp }
           { cases' n with n; repeat simp }
-        cases' h0 (seq m x) with h0 h1
+        cases' h0 (seq b m x) with h0 h1
         {
           have h1 := seq_pos hmgt hm x
           rw [h0] at h1
@@ -162,10 +156,10 @@ lemma seq_one {m} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) : ∃ k, seq m k = 1 :
           {
             rw [seq_one_next h1]
             rw [h1] at hx
-            exact Ne.lt_of_le' hb hx
+            exact Ne.lt_of_le' hu hx
           }
           {
-            have hxs : seq m x.succ < seq m x := seq_dec hmgt hm hg1
+            have hxs : seq b m x.succ < seq b m x := seq_dec hmgt hm hg1
             exact Nat.lt_of_lt_of_le hxs hx
           }
         }
@@ -175,12 +169,12 @@ lemma seq_one {m} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) : ∃ k, seq m k = 1 :
     { apply seq_lt_seq_zero hmgt hm }
     { exact 0 }
   have ⟨l, hl⟩ := seq_le_1
-  have hseq_pos : seq m l > 0 := seq_pos hmgt hm l
+  have hseq_pos : seq b m l > 0 := seq_pos hmgt hm l
   use l
   exact Nat.le_antisymm hl hseq_pos
 
-lemma iterate' {a m x y : ℕ} (hmgt : m > 0) (hf : f a x ≡ f a y [MOD m]) (hm : 3^m ≡ 1 [MOD m])
-  (h : x ≡ y [MOD order m 3]) : x ≡ y [MOD m] := by
+lemma iterate' {a b m x y : ℕ} (hmgt : m > 0) (hf : f a b x ≡ f a b y [MOD m]) (hm : b^m ≡ 1 [MOD m])
+  (h : x ≡ y [MOD order m b]) : x ≡ y [MOD m] := by
   wlog hge : x >= y
   {
     have hyx : y >= x := Nat.le_of_not_le hge
@@ -191,39 +185,39 @@ lemma iterate' {a m x y : ℕ} (hmgt : m > 0) (hf : f a x ≡ f a y [MOD m]) (hm
   }
 
   unfold f at hf
-  let d := order m 3
+  let d := order m b
 
   have hdgt : d > 0 := seq_pos hmgt hm 1
   have ⟨t, hdt⟩ : ∃ t, x = y + t * d := (Helper.add_fac_iff_mod hdgt hge).mp h
-  have h3dm : 3^d ≡ 1 [MOD m] := seq_pow_mod_prev m 0
-  have ha3xym : a * 3^x ≡ a * 3^y [MOD m] := by
+  have hbdm : b^d ≡ 1 [MOD m] := seq_pow_mod_prev b m 0
+  have habxym : a * b^x ≡ a * b^y [MOD m] := by
     rw [hdt, pow_add]
-    have h0 : 3^y = 3^y * 1^t := by simp
+    have h0 : b^y = b^y * 1^t := by simp
     nth_rewrite 2 [h0]
     repeat apply Nat.ModEq.mul_left
     rw [mul_comm, pow_mul]
-    exact Nat.ModEq.pow t h3dm
+    exact Nat.ModEq.pow t hbdm
 
-  exact Nat.ModEq.add_left_cancel ha3xym hf
+  exact Nat.ModEq.add_left_cancel habxym hf
 
-lemma iterate {a m x y k : ℕ} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) (hf : f a x ≡ f a y [MOD m])
-  (h : x ≡ y [MOD seq m k.succ]) : x ≡ y [MOD seq m k] := by
-  have hsmk : 3^seq m k ≡ 1 [MOD seq m k] := seq_pow_one hm k
-  have hpos : seq m k > 0 := seq_pos hmgt hm k
-  have hdvd : seq m k ∣ m := seq_dvd_seq_zero hm k
-  have hfsmk : f a x ≡ f a y [MOD seq m k] := by
+lemma iterate {a b m x y k : ℕ} (hmgt : m > 0) (hm : b^m ≡ 1 [MOD m]) (hf : f a b x ≡ f a b y [MOD m])
+  (h : x ≡ y [MOD seq b m k.succ]) : x ≡ y [MOD seq b m k] := by
+  have hsmk : b^seq b m k ≡ 1 [MOD seq b m k] := seq_pow_one hm k
+  have hpos : seq b m k > 0 := seq_pos hmgt hm k
+  have hdvd : seq b m k ∣ m := seq_dvd_seq_zero hm k
+  have hfsmk : f a b x ≡ f a b y [MOD seq b m k] := by
     have ⟨d, hd⟩ := hdvd
     nth_rewrite 1 [hd] at hf
     exact Nat.ModEq.of_mul_right d hf
   exact iterate' hpos hfsmk hsmk h
 
-theorem bijection_mod_m {a m x y : ℕ} (hmgt : m > 0) (hm : 3^m ≡ 1 [MOD m]) (hf : f a x ≡ f a y [MOD m])
+theorem bijection_mod_m {a b m x y : ℕ} (hmgt : m > 0) (hm : b^m ≡ 1 [MOD m]) (hf : f a b x ≡ f a b y [MOD m])
   : x ≡ y [MOD m] := by
   have hxy1 : x ≡ y [MOD 1] := Nat.modEq_one
   have ⟨idx, hidx⟩ := seq_one hmgt hm
 
   let r := Nat.lt
-  let C := fun k => x ≡ y [MOD seq m k]
+  let C := fun k => x ≡ y [MOD seq b m k]
   have : WellFounded r := by apply Nat.lt_wfRel.wf
   have well_ordering := @WellFounded.induction_bot ℕ r this idx 0 C
 
