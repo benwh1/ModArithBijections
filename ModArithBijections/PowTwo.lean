dba2ff58019c2684a3c75570a6487277b5ba3aa4
Ben Whitmore <32911992+benwh1@users.noreply.github.com>
2023-11-06 22:50:09 +0000

    Update proof of m = 2^k special case to work with any a*b^n+c*n with odd b

--- ModArithBijections/PowTwo.lean
+++ ModArithBijections/PowTwo.lean
@@ -1,11 +1,22 @@
 import ModArithBijections.ModM
 
-lemma three_pow_two_pow_k : ∀ k, 3^2^k ≡ 1 [MOD 2^(k+1)] := by
+lemma odd_pow_two_pow_k {b : ℕ} (hb : Odd b) : ∀ k, b^2^k ≡ 1 [MOD 2^(k+1)] := by
   intro k
-  apply (Helper.add_fac_iff_mod (Nat.one_le_pow' _ _) (Nat.one_le_pow' _ _)).mpr
+  have ⟨l, hl⟩ := hb
+  have hb2kpos : b^2^k > 0 := by
+    rw [hl]
+    apply Nat.one_le_pow' _ (2 * l)
+  apply (Helper.add_fac_iff_mod (Nat.one_le_pow' _ _) hb2kpos).mpr
+
+  clear hb2kpos
 
   induction' k with k hind
-  { use 1; simp }
+  {
+    use l;
+    rw [hl];
+    simp;
+    ring
+  }
   {
     have ⟨t, ht⟩ := hind
     use t * (1 + t * 2^k)
@@ -13,10 +24,11 @@ lemma three_pow_two_pow_k : ∀ k, 3^2^k ≡ 1 [MOD 2^(k+1)] := by
     ring
   }
 
-lemma three_pow_two_pow_k' : ∀ k, 3^2^k ≡ 1 [MOD 2^k] := by
+lemma odd_pow_two_pow_k' {b : ℕ} (hb : Odd b) : ∀ k, b^2^k ≡ 1 [MOD 2^k] := by
   intro k
-  have h := three_pow_two_pow_k k
+  have h := odd_pow_two_pow_k hb k
   exact Nat.ModEq.of_mul_right 2 h
 
-theorem bijection_mod_pow_two {x y k} (hf : f x ≡ f y [MOD 2^k]) : x ≡ y [MOD 2^k] :=
-  bijection_mod_m (Nat.one_le_pow' _ _) (three_pow_two_pow_k' k) hf
+theorem bijection_mod_pow_two {a b c k x y} (hb : Odd b) (hc : Nat.Coprime c (2^k))
+  (hf : f a b c x ≡ f a b c y [MOD 2^k]) : x ≡ y [MOD 2^k] :=
+  bijection_mod_m (Nat.one_le_pow' _ _) (odd_pow_two_pow_k' hb k) hc hf
